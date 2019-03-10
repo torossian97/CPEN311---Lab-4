d209f5ce5b3f2f5f02ebaa3af51c413512bde6c4
Antoine Torossian <torossian97@gmail.com>
2019-03-09 19:37:51 -0800

    added comments and bug fixes

--- template_de1soc/brute_force.sv
+++ template_de1soc/brute_force.sv
@@ -1,12 +1,13 @@
 module brute_force(input logic clk,
 						 input logic start,
 						 input logic [255:0] decoded_data,
+						 output logic [23:0] tester_key,
 						 output logic [23:0] secret_key,
 						 output logic finish);
 
 logic [5:0] lim = 0;
 logic [3:0] state;
-logic [21:0] intermediate_key = 0;
+logic [22:0] intermediate_key = 0;
 logic [255:0] dd;
 
 parameter IDLE = 4'b1000;
@@ -21,22 +22,21 @@ always_ff @(posedge clk) begin
 
 	case(state)
 		IDLE: begin
-					lim <= 0;
-					dd <= decoded_data;
-					//safety check
-					if(intermediate_key == {22{1'b1}}) intermediate_key <= 0;
-					//start
+					lim <= 0; // keeps track of when the end of the 256 bit data has been completely read; up to a value of 31 ([31:0])
+					dd <= decoded_data; // assign input to logic so it can be modified later
+					//if(intermediate_key == {22{1'b1}})  // should apply safety check for infinite loop
 					if(start == 1) state <= INCREMENT_KEY;
 				end
 		INCREMENT_KEY: begin
-								intermediate_key <= intermediate_key + 1;
+								intermediate_key <= intermediate_key + 1; // brute-force, trying everythign in the key space; starts at 1
 								state <= SEND_KEY;
 							end
 		SEND_KEY: begin
-						if(start == 1) state <= VALIDATOR;
+						tester_key <= {2'b0, intermediate_key[21:0] - 1}; // -1 because we want to start at index 0, [21:0] because 22 is overflow protection
+						if(start == 1) state <= VALIDATOR; // waiting for other state machine to say "yes, received key and fecthed data"
 					 end
 		VALIDATOR: begin
-						if((dd[7:0] >= 8'd97 && dd[7:0] <= 8'd122) || dd[7:0] == 8'd32) begin
+						if((dd[7:0] >= 8'd97 && dd[7:0] <= 8'd122) || dd[7:0] == 8'd32) begin // check each char to know if real value *[a:z] and space*
 							state <= SHIFTER;
 						end
 						else begin
@@ -44,14 +44,14 @@ always_ff @(posedge clk) begin
 						end
 					  end
 		SHIFTER: begin
-						dd <= dd >> 8;
-						if(lim == 8'd31) state <= OUTPUT;
+						dd <= dd >> 8; // shift data down 8 bits so VALIDATOR can read the next 8 bits (the next character)
+						if(lim == 8'd31) state <= OUTPUT; // once 32 iterations have occured successfully, go to OUTPUT state
 						else state <= VALIDATOR;
 						
 						lim <= lim + 1;
 					end
 		OUTPUT: begin
-					secret_key = {2'b0, intermediate_key};
+					secret_key = {2'b0, intermediate_key[21:0] - 1}; // -1 because we want to start at index 0, [21:0] because 22 is overflow protection
 					state <= IDLE;
 				  end
 		default: state <= IDLE;
